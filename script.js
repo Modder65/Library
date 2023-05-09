4ac201ed47a855c1b28b1c9b0a245d337cb58fa2
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-05-09 16:35:47 -0400

    changed to class syntax in JavaScript

--- script.js
+++ script.js
@@ -22,80 +22,70 @@ let currentIndex = 0;
 //placeholder variable that holds a boolean value which later determines the color for the top-border of the created "card"
 let cardColor;
 
-//object constructor that defines the parameters for each book 
-function Book(title, author, currentStatus, color) {
-    this.title = title;
-    this.author = author;
-    this.currentStatus = currentStatus;
-    this.color = color;
-}
 
-//object method that creates a new "card" containg the new books information which is displayed to the page
-Book.prototype.createCard = function () {
-
-    let textContainer = document.createElement('div');
-    let newCard = document.createElement('div')
-    newCard.setAttribute("data-index", (myLibrary.length - 1));
-    let theIndex = newCard.getAttribute("data-index");
-    let titleText = document.createElement('p');
-    let authorText = document.createElement('p');
-    let imgContainer = document.createElement('div');
-    let btnContainer = document.createElement('div');
-    let btnRead = document.createElement('button');
-    let btnNotRead = document.createElement('button');
-    if (cardColor == true) {
-        newCard.style.borderColor = "green";
-        myLibrary[theIndex].color = "green";
-    } else if (cardColor == false) {
-        newCard.style.borderColor = "red";
-        myLibrary[theIndex].color = "red";
+//class that allows a new instance of Book to be constructed, appended to the myLibrary array, and displayed on the screen
+class Book {
+    constructor(title, author, currentStatus, color) {
+        this.title = title;
+        this.author = author;
+        this.currentStatus = currentStatus;
+        this.color = color;
     }
-    removeCard = document.createElement('img');
-    newCard.className = "book";
-    /*newCard.id = "newCard";*/
-    textContainer.className = "textContainer";
-    imgContainer.className = "imgContainer";
-    removeCard.setAttribute("src", "imgs/book-remove-outline.svg");
-    removeCard.setAttribute("onclick", "NewDeletion(this)");
-    removeCard.id = "removeCard";
-    btnContainer.className = "btnContainer";
-    btnRead.className = "btnRead";
-    btnRead.setAttribute("onclick", "read(this);");
-    btnNotRead.setAttribute("onclick", "notRead(this);")
-    btnNotRead.className = "btnNotRead";
-    books.appendChild(newCard);
-    newCard.appendChild(textContainer);
-    newCard.appendChild(btnContainer);
-    btnContainer.appendChild(btnRead);
-    btnContainer.appendChild(btnNotRead);
-    textContainer.appendChild(titleText);
-    textContainer.appendChild(authorText);
-    newCard.appendChild(imgContainer);
-    imgContainer.appendChild(removeCard);
-    titleText.textContent = title.value;
-    authorText.textContent = `by: ${author.value}`;
-    btnRead.textContent = "Read";
-    btnNotRead.textContent = "Not Read";
-}
 
-//function that creates a new instance of the Book() constructor and appends it to the myLibrary array
-function addBookToLibrary() {
-    let newBook = new Book(bookTitle.value, bookAuthor.value, bookStatus.value);
-    if (newBook.currentStatus == "read") {
-        cardColor = true;
-    } else if (newBook.currentStatus == "not read") {
-        cardColor = false;
+    createCard() {
+        let textContainer = document.createElement('div');
+        let newCard = document.createElement('div')
+        newCard.setAttribute("data-index", (myLibrary.length - 1));
+        let theIndex = newCard.getAttribute("data-index");
+        let titleText = document.createElement('p');
+        let authorText = document.createElement('p');
+        let imgContainer = document.createElement('div');
+        let btnContainer = document.createElement('div');
+        let btnRead = document.createElement('button');
+        let btnNotRead = document.createElement('button');
+        if (cardColor == true) {
+            newCard.style.borderColor = "green";
+            myLibrary[theIndex].color = "green";
+        } else if (cardColor == false) {
+            newCard.style.borderColor = "red";
+            myLibrary[theIndex].color = "red";
+        }
+        let removeCard = document.createElement('img');
+        newCard.className = "book";
+        textContainer.className = "textContainer";
+        imgContainer.className = "imgContainer";
+        removeCard.setAttribute("src", "imgs/book-remove-outline.svg");
+        removeCard.setAttribute("onclick", "NewDeletion(this)");
+        removeCard.id = "removeCard";
+        btnContainer.className = "btnContainer";
+        btnRead.className = "btnRead";
+        btnRead.setAttribute("onclick", "read(this)");
+        btnNotRead.setAttribute("onclick", "notRead(this)")
+        btnNotRead.className = "btnNotRead";
+        books.appendChild(newCard);
+        newCard.appendChild(textContainer);
+        newCard.appendChild(btnContainer);
+        btnContainer.appendChild(btnRead);
+        btnContainer.appendChild(btnNotRead);
+        textContainer.appendChild(titleText);
+        textContainer.appendChild(authorText);
+        newCard.appendChild(imgContainer);
+        imgContainer.appendChild(removeCard);
+        titleText.textContent = title.value;
+        authorText.textContent = `by: ${author.value}`;
+        btnRead.textContent = "Read";
+        btnNotRead.textContent = "Not Read";
     }
-    myLibrary.push(newBook);
-}
 
-//function that reveals/hides a form on the page that the user will use to enter the new books information
-function changeDisplay() {
-    if (content.style.display = "none") {
-        content.style.display = "flex";
-    } else if (content.style.display = "flex") {
-        content.style.display = "none";
+    addBookToLibrary() {
+        if (this.currentStatus == "read") {
+            cardColor = true;
+        } else if (this.currentStatus == "not read") {
+            cardColor = false;
+        }
+        myLibrary.push(this);
     }
+
 }
 
 //function to delete and rearrange the cards on the page according to their correct data-index attribute values in correlation to the index of their corresponding object in the array
@@ -127,7 +117,6 @@ function NewDeletion(theThis) {
     displayArray();
 }
 
-
 //function that changes the status of a book to read, and changes the border-color to accurately represent the status
 function read(theThis) {
     let theIndex = theThis.parentNode.parentNode.getAttribute('data-index');
@@ -144,6 +133,15 @@ function notRead(theThis) {
     myLibrary[theIndex].color = "red";
 }
 
+//function that reveals/hides a form on the page that the user will use to enter the new books information
+function changeDisplay() {
+    if (content.style.display = "none") {
+        content.style.display = "flex";
+    } else if (content.style.display = "flex") {
+        content.style.display = "none";
+    }
+}
+
 //event listener that calls the changeDisplay() function when the addBook image is clicked
 addBook.addEventListener('click', function() {
     changeDisplay();
@@ -153,14 +151,12 @@ addBook.addEventListener('click', function() {
 submit.addEventListener('click', function(event) {
     let counter = 0;
     if (bookTitle.value == '' || bookAuthor.value == '') {
-        
+        // do nothing
     } else {
         event.preventDefault()
-        addBookToLibrary();
+        let newBook = new Book(bookTitle.value, bookAuthor.value, bookStatus.value);
+        newBook.addBookToLibrary();
         myLibrary[counter].createCard();
         counter++;
     }
 });
-
-
-
